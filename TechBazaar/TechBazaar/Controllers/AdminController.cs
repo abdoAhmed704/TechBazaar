6200de69083106a049f6c2052f219091a9c7fcd9
Osama <osama0128000@gmail.com>
2025-05-11 17:35:43 +0300

    ..

--- TechBazaar/TechBazaar/Controllers/AdminController.cs
+++ TechBazaar/TechBazaar/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using TechBazaar.Core.Enums;
 using TechBazaar.Core.Interfaces;
@@ -7,6 +8,7 @@ using TechBazaar.Ef.Repository;
 
 namespace TechBazaar.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
         private readonly IUnitOfWork unitOfWork;
